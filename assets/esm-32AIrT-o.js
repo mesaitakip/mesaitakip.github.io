1818e82a9ec64c7bd0dbb239cde884abe7be8712
efek0349 <kndmrefe@gmail.com>
2026-06-27 05:54:46 +0300

    Updates

--- assets/esm-32AIrT-o.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BY9kiBm0.js","assets/dist-BBuC9Xni.js"])))=>i.map(i=>d[i]);
-import{i as e}from"./dist-BBuC9Xni.js";import{t}from"./preload-helper-zJ_50EbN.js";var n=e(`App`,{web:()=>t(()=>import(`./web-BY9kiBm0.js`).then(e=>new e.AppWeb),__vite__mapDeps([0,1]))});export{n as App};
