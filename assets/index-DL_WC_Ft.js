2e788df887d7cf861e5e07340c2386e3b8699c47
efek0349 <kndmrefe@gmail.com>
2026-07-05 19:16:39 +0300

    Updates

--- assets/index-DL_WC_Ft.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C-wW8lvC.js","assets/index-3WEx2qP1.js","assets/index-CTH9eoe7.css"])))=>i.map(i=>d[i]);
-import{ai as p,_ as r}from"./index-3WEx2qP1.js";const _=p("App",{web:()=>r(()=>import("./web-C-wW8lvC.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
