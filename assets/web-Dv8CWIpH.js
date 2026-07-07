8221dd5d08ceffee1923e44b38935202e00a0893
efek0349 <kndmrefe@gmail.com>
2026-07-07 23:16:55 +0300

    Updates

--- assets/web-Dv8CWIpH.js
+++
@@ -1,1 +0,0 @@
-import{ap as n}from"./index-CBcaTZbu.js";class r extends n{async alert(e){window.alert(e.message)}async prompt(e){const a=window.prompt(e.message,e.inputText||"");return{value:a!==null?a:"",cancelled:a===null}}async confirm(e){return{value:window.confirm(e.message)}}}export{r as DialogWeb};
